6ac4157d58d2b6207ace05369036b7d180a4a8cf
Brian D'Ostilio <briand787b@gmail.com>
2017-03-28 18:16:02 -0400

    added http_form

---
+++ 65_httpForm/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+	"strings"
+	"log"
+	"strconv"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+type user struct {
+	UserName string
+	Password string
+}
+
+// Map sessions to validated users
+var usrSessn = make(map[string]*user)
+var sIndex int
+var db *sql.DB
+
+// Global template variable
+var tpl *template.Template
+
+func init() {
+	// Connect to the database
+	db, _ = sql.Open("mysql", "root:P@ssword123!@/second_db")
+	err := db.Ping()
+	if err != nil {
+		log.Fatal("error connecting to database: ", err)
+	}
+
+	// Parse templates
+	tpl = template.Must(template.ParseGlob("*.html"))
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	// Print the ip address of the user to the console
+	userIP := strings.Split(r.RemoteAddr, ":")[0]
+	fmt.Println(userIP)
+	rows, err := db.Query(`INSERT INTO request_ips (ip, date) VALUES (?, ?)`, userIP, time.Now())
+	if err != nil {
+		fmt.Println("error querying database: ", err)
+	} else {
+		fmt.Println("rows from db: ", rows)
+	}
+
+	// Work on form
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	c, err := r.Cookie("session")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Hello %s", c.Value)
+	un, ok := usrSessn[c.Value]
+	if ok {
+		fmt.Fprintf(w, "Hello %s", un)
+	} else {
+		fmt.Fprint(w, "Hey, we couldn't find you in the map")
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method: ", r.Method)
+	if r.Method == "GET" {
+		c, err := r.Cookie("session")
+		if err != nil {
+			fmt.Println("Error reading cookie ", err)
+		} else {
+			fmt.Println("cookie: ", c.Value)
+		}
+		err = tpl.ExecuteTemplate(w, "login.html", nil)
+		if err != nil {
+			fmt.Print("error: ", err)
+		}
+	} else {
+		r.ParseForm()
+		fmt.Println("This is the better way to get the value from the form, since url.Values is a map of strings to slice of strings", r.Form.Get("username"))
+		fmt.Println("username:", r.Form.Get("username"))
+		fmt.Println("password:", r.Form.Get("password"))
+		sIndex++
+		fmt.Println("This is the value of sIndex: ", string(sIndex))
+		http.SetCookie(w, &http.Cookie{Name:"session", Value:strconv.Itoa(sIndex)})
+		usrSessn[string(sIndex)] = &user{UserName:r.Form["username"][0], Password:r.Form["password"][0]}
+	}
+}
+
+func handleFavicon(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handled favicon request")
+}
+
+func main() {
+	defer db.Close()
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/favicon.ico/", handleFavicon)
+	http.HandleFunc("/", sayhelloName)
+
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
